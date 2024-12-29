258c34afa3946ee4a84eb0adaa400cf0ef79e47d
Mateus Peres <mateuspereslopesl@outlook.com>
2024-12-29 13:28:03 -0300

    feature: update profile redirection and add logout route at index

--- app/Views/index.php
+++ app/Views/index.php
@@ -70,8 +70,13 @@
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Peres Imóveis</span>
         </a>
         <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <form action="<?= base_url('public/profile') ?>" method="get">
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Perfil </button>
+
+          <form action="<?= base_url('public/profile'); ?>" method="get">
+            <button type="submit" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
+              <img class="w-12 h-12 rounded-full"
+                src="<?= isset($profile_photo) && $profile_photo ? base_url('public/uploads/profile_photos/' . esc($profile_photo['file_name'])) : base_url('public/uploads/profile_photos/default.png') ?>"
+                alt="user photo">
+            </button>
           </form>
 
           <button onclick="toggleMenu()" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
